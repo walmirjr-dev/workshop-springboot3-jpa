ff9240380acb2282f08b4e66905bd192ac6633fc
Walmir Alves <walmirjr915@gmail.com>
2025-08-25 18:22:26 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/OrderItem.java
+++ src/main/java/com/example/course/entities/OrderItem.java
@@ -66,6 +66,10 @@ public class OrderItem implements Serializable{
 		id.setProduct(product);
 	}
 
+	public Double getSubTotal() {
+		return quantity * price;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
