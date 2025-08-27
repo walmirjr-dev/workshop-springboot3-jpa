821f0f5f9a8710730d3c86cf26a4f605cef8de9e
Walmir Alves <walmirjr915@gmail.com>
2025-08-27 01:10:18 -0300

    OrderResource and OrderService implementation

--- src/main/java/com/example/course/services/OrderService.java
+++ src/main/java/com/example/course/services/OrderService.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entities.Order;
 import com.example.course.repositories.OrderRepository;
+import com.example.course.services.exceptions.DatabaseException;
+import com.example.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class OrderService {
@@ -21,7 +26,41 @@ public class OrderService {
 
 	public Order findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
+
+	public Order insert(Order obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+
+		if (!repository.existsById(id)) {
+	        throw new ResourceNotFoundException(id);
+	    }
+
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Order update(Long id, Order obj) {
+
+		try {
+			Order entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Order entity, Order obj) {
+		entity.setMoment(obj.getMoment());
+		entity.setOrderStatus(obj.getOrderStatus());
+	}
+
 
 }
